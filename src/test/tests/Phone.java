a5386bc7495edfb7e60d60cc3fcfb2804f9e6ff9
botagozbaizhambayeva <sulu.bota@gmail.com>
2023-03-15 12:38:58 -0500

    initial commit

---
+++ src/test/tests/Phone.java
@@ -0,0 +1,10 @@
+package test.tests;
+
+import test.utilities.Driver;
+
+public class Phone {
+
+    public static void main(String[] args) {
+        Driver.getDriver().get("https://google.com");
+    }
+}
